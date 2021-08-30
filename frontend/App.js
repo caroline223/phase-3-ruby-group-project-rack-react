26671402706575b5a160b4030061c709323d6376
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-29 21:38:29 -0400

    Updated files

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Books from "../src/components/BooksComponents/Books"
+import Authors from './components/AuthorsComponents/Authors';
+import NewTaskForm from "./components/NewTaskForm"
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/books" component={Books}></Route>
+        <Route exact path="/authors" component={Authors}></Route>
+       
+      </Switch>
+    </Router>
+  ) ;
+}
+
+export default App;
