aba326645e89debc858ff4c7ea3bfc99346f8d8b
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-29 22:30:30 -0400

    redownloaded semantic-ui-react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+//import Books from "../src/components/BooksComponents/Books"
+import Authors from './Components/AuthorsComponents/Authors';
+//import NewTaskForm from "./components/NewTaskForm"
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Switch>
+      
+        <Route exact path="/authors" component={Authors}></Route>
+        
+      </Switch>
+  </Router>
   );
 }
 
