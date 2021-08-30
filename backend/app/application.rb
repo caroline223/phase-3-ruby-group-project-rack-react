e9fb32e270c1c9916dbb1e5582ecf7662ec1ef4f
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-29 21:52:03 -0400

    Updated the application.rb file to include a path for the author requests file

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -4,8 +4,30 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+    if req.path.match(/books/) && req.get?
+      books = Book.all
+      return [200, { 'Content-Type' => 'application/json' }, [ books.to_json ]]
+      #message: taking a message hash and converting it to json server
+
+    elsif req.path.match(/authors/) && req.get?
+      authors = Author.all
+      return [200, { 'Content-Type' => 'application/json' }, [ authors.to_json ]]
+
+    elsif req.path.match(/arequests/) && req.get?
+      arequests = AuthorRequest.all
+      return [200, { 'Content-Type' => 'application/json' }, [ arequests.to_json ]]
+
+    elsif req.path.match(/books/) && req.post?
+     data = JSON.parse  req.body.read #takes sthg that is json and parse it and turn it into ruby
+     book = Book.create(data)
+     return [200, { 'Content-Type' => 'application/json' }, [ book.to_json ]]
+
+
+    elsif req.delete?
+      id = req.path_info.split("/books/").last
+      book = Book.find_by(id)
+      book.delete
+      return [200, { 'Content-Type' => 'application/json' }, [ {message: "Book deleted"}.to_json ]]
 
     else
       resp.write "Path Not Found"
