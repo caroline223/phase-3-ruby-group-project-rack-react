bd2d76b0d8f2f797493ce89a6a98e9964c684198
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-29 23:21:13 -0400

    Added the home page component to the components file

---
+++ frontend/src/Components/HomePage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Card, Container } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+class HomePage extends React.Component {
+
+
+    render() {
+
+        return(
+            <div>
+                <h1>Sunnyville Public Library</h1>
+                <h3>Welcome!</h3>
+                <p>Please choose an option below to search for books!</p>
+                <Container>
+                    <Card>
+                        <Card.Content>
+                            <Link to={"/books"}>Books</Link>
+                        </Card.Content>
+                    </Card>
+                    <Card>
+                        <Card.Content>
+                            <Link to={"/authors"}>Authors</Link>
+                        </Card.Content>
+                    </Card>
+                </Container>
+            </div>
+        )
+    }
+
+
+}
+export default HomePage;
