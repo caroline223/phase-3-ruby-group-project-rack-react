d8646e53f0b9db98c467072caf89a07ca6918def
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-31 22:10:22 -0400

    Added methods to the author.rb and book.rb files

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,3 +1,21 @@
 class Book < ActiveRecord::Base 
     belongs_to :author
+
+
+   def self.all_with_authors
+    
+    self.all.map do |book|
+        { 
+        title: book.title,
+        genre: book.genre,
+        author_name: book.author.full_name,
+        publishing_date: book.publishing_date,
+        rating: book.rating,
+        image_url: book.image_url,
+        author_id: book.author_id
+        }
+     end
+   end
+   
+
 end
