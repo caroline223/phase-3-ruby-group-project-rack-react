26933a1c3451889d7b61a123b120a8b8945166be
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-09-02 01:34:19 -0400

    Added the  dynamic routes to the AuthorsForm, BooksForm, as well as the individualAuthor components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Books from "./Components/BooksComponents/Books"
 import Authors from './Components/AuthorsComponents/Authors';
 import AuthorsForm from './Components/AuthorsComponents/AuthorsForm'
+import BooksForm from './Components/BooksComponents/BooksForm';
+import IndividualAuthor from './Components/AuthorsComponents/IndividualAuthor';
 import HomePage from './Components/HomePage';
+
 //import NewTaskForm from "./components/NewTaskForm"
 
 
@@ -14,8 +17,10 @@ function App() {
       <Switch>
         <Route exact path="/home" component={HomePage}></Route>
         <Route exact path="/books" component={Books}></Route>
-        <Route exact path="/authors" component={Authors}></Route> 
+        <Route exact path="/authors" component={Authors}></Route>
+        <Route exact path="/authors/:id" component={IndividualAuthor}></Route> 
         <Route exact path="/arequests" component={AuthorsForm}></Route> 
+        <Route exact path="/brequests" component={BooksForm}></Route> 
       </Switch>
   </Router>
   );
