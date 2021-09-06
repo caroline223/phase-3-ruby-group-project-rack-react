845a780b20458019f52dc764052d548f0ace90b3
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-09-06 19:20:22 -0400

    Configured the application.rb file to include the DELETE request

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -25,56 +25,45 @@ class Application
       author =  Author.create(data)
       return [200, { 'Content-Type' => 'application/json' }, [author.to_json ]]
 
-
-   
-    elsif @req.path.match(/books/) && @req.get? && params_id
-      author = Author.find_by_id(params_id)
-      author_and_books = {
-        name: author.full_name,
-        books: author.books 
-      }
-      return [200, { 'Content-Type' => 'application/json' }, [ author_and_books.to_json ]] 
-
-      
     elsif @req.path.match(/books/) && @req.get?
       return [200, { 'Content-Type' => 'application/json' }, [ Book.all_with_authors.to_json ]]
+    
+    elsif @req.path.match(/books/) && @req.get? && params_id
+      book = Book.find_by_id(params_id)
+      book_info = {
+        id: book.id,
+        title: book.title,
+        genre: book.genre,
+        author_name: book.author.full_name,
+        publishing_date: book.publishing_date,
+        rating: book.rating,
+        author_id: book.author_id
+      }
+      return [200, { 'Content-Type' => 'application/json' }, [ book_info.to_json ]] 
 
-    elsif @req.path.match(/books/) && @req.post?
+    elsif @req.path.match(/books/) && @req.delete? 
+      id = @req.path_info.split('/books/').last
+      book = Book.find_by_id(id)
+      book.delete
+      return [200, { 'Content-Type' => 'application/json' }, [ {message: "Book deleted"}.to_json ]]
+   
+   
+    elsif @req.path.match(/brequests/) && @req.get?
+      book = Novel.all
+      return [200, { 'Content-Type' => 'application/json' }, [ book.to_json ]]
+  
+    
+    elsif @req.path.match(/brequests/) && @req.post?
       data = JSON.parse  @req.body.read 
-      book =  Book.create(data)
+      book =  Novel.create(data)
       return [200, { 'Content-Type' => 'application/json' }, [book.to_json ]]
      
-      
-
-    elsif @req.path.match(/arequests/) && @req.get?
-      author = AuthorRequest.all
-      return [200, { 'Content-Type' => 'application/json' }, [ author.to_json ]]
-  
-      
-    elsif @req.path.match(/arequests/) && @req.post?
-        data = JSON.parse  @req.body.read 
-        author_request =  AuthorRequest.create(data)
-        return [200, { 'Content-Type' => 'application/json' }, [author_request.to_json ]]
 
 
-      elsif @req.path.match(/brequests/) && @req.get?
-        book = BookRequest.all
-        return [200, { 'Content-Type' => 'application/json' }, [ book.to_json ]]
-    
-        
-      elsif @req.path.match(/brequests/) && @req.post?
-          data = JSON.parse  @req.body.read 
-          book_request =  BookRequest.create(data)
-          return [200, { 'Content-Type' => 'application/json' }, [book_request.to_json ]]
-
-
-
-
-
-    elsif @req.delete?
-      id = @req.path_info.split("/books/").last
-      book = Book.find_by(id)
-      book.delete
+    elsif @req.path.match(/brequests/) && @req.delete? 
+      id = @req.path_info.split('/brequests/').last
+      novel = Novel.find_by_id(id)
+      novel.delete
       return [200, { 'Content-Type' => 'application/json' }, [ {message: "Book deleted"}.to_json ]]
 
     else
